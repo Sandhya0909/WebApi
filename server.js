d0ed293f41a3c453b5901e81d77486ac8748b898
Sandhya0909 <kittusandhya4@gmail.com>
2024-10-28 10:49:02 +0100

    'first'

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+
+// main function that logs "hello world"
+//function main(){
+    //console.log("hello world");
+//}
+
+// call the main function to execute it
+//main();
+
+
+// server.js
+
+const endpoint = require('./another');
+function main()
+{
+    console.log('Hello, World!');
+    console.log(endpoint());
+}
+main{};
